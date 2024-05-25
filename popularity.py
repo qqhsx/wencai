4609f7093c4bc9117a360f8973f9a9f6f7a15e62
qqhsx <56535272+qqhsx@users.noreply.github.com>
2024-05-25 17:24:56 +0800

    Update popularity.py

--- popularity.py
+++ popularity.py
@@ -1,9 +1,8 @@
-import os
 import pywencai
 
-# 从环境变量读取查询词
-query = os.getenv('CSV_FILE_NAME ', '退市股票')  # 如果环境变量不存在，则使用默认值 '退市股票'
-filename = f'{CSV_FILE_NAME }'
+# 定义变量
+query = '退市股票'
+filename = f'{query}.csv'
 
 # 使用变量进行查询和保存结果
 res = pywencai.get(question=query, loop=True)
