e8c6ccc8cf9e644d1b55a7317e23c62fc714ce77
qqhsx <56535272+qqhsx@users.noreply.github.com>
2024-05-25 11:17:37 +0800

    Update popularity.py

--- popularity.py
+++ popularity.py
@@ -1,4 +1,4 @@
 import pywencai
 res = pywencai.get(question='退市股票', loop=True)
-res.to_csv('问财.csv', index=False, encoding='utf-8-sig')
+res.to_csv('退市股票.csv', index=False, encoding='utf-8-sig')
 print(res)
