32164fddce3a011118af1ea835a49b44c0d75f65
qqhsx <56535272+qqhsx@users.noreply.github.com>
2024-05-27 09:53:25 +0800

    Update popularity.py

--- popularity.py
+++ popularity.py
@@ -2,11 +2,11 @@ import pywencai
 
 # 定义变量
 query = '退市股票'
-# filename = f'{query}.csv'
+filename = f'{query}.csv'
 
 # 使用变量进行查询和保存结果
 res = pywencai.get(question=query, loop=True)
-# res.to_csv(filename, index=False, encoding='utf-8-sig')
+res.to_csv(filename, index=False, encoding='utf-8-sig')
 
 # 打印结果
 print(res)
