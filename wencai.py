3646820f9fbd308b3750430bd9d89ff050d395ab
qqhsx <56535272+qqhsx@users.noreply.github.com>
2024-08-31 08:38:33 +0800

    Update wencai.py

--- wencai.py
+++ wencai.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd  # 导入 pandas 库
 
 # 获取关键字列表
+# 在Settings-Actions中创建一个New repository secret，名称为“KEYWORD”。秘密为"人气排名,涨停,退市股票"
 keywords = os.environ.get("KEYWORD", "").split(',')
 
 # 遍历关键字列表进行查询和保存结果
